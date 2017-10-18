95bac3b8145871fa7b5a1abe6518fe39aeb7379e
no <noemie.scherer@gmail.com>
2017-10-18 09:24:40 +0200

    still working on billboard death

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    public Camera m_Camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
---
+++ Assets/Scripts/DieWhenFinishedAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieWhenFinishedAnim : MonoBehaviour {
+
+    private Animation death;
+
+	// Use this for initialization
+	void Start () {
+        death = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (death["death"].time >= death["death"].length) {
+            Destroy(GetComponentInParent<GameObject>());
+
+        }
+
+    }
+}
