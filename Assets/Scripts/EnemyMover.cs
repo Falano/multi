275f7d6fb2e863c9c7d22ce1a64d8756b310490d
no <noemie.scherer@gmail.com>
2017-10-18 11:33:21 +0200

    fixed all mice anim issues but yknow actually lan doesn't work :P

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -9,7 +9,6 @@ public class EnemyMover : MonoBehaviour
     public Vector3 goal;
     NavMeshHit hit;
     public Vector3 lvlSize;
-    bool moving;
     Animator animator;
     public Vector2 waitRange = new Vector2(3,6);
     public int rotationSpeed = 5;
@@ -17,10 +16,11 @@ public class EnemyMover : MonoBehaviour
 
     void Start()
     {
+        lvlSize = GameObject.FindGameObjectWithTag("ColorManager").GetComponent<ColorManager>().LvlSize;
         ag = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        ChangeDestination();
-        lvlSize = GameObject.FindGameObjectWithTag("ColorManager").GetComponent<ColorManager>().LvlSize;
+        IEnumerator wait = waitForChangeDir(Random.Range(waitRange.x, waitRange.y));
+        StartCoroutine(wait);        
     }
 
     void Update()
@@ -32,18 +32,6 @@ public class EnemyMover : MonoBehaviour
         }
 
         Debug.DrawLine(transform.position, goal, Color.red);
-
-        /*
-        if ( transform.forward != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(
-                transform.rotation,
-                Quaternion.LookRotation(transform.forward),
-                Time.deltaTime * rotationSpeed
-            );
-        }
-        */
-
     }
 
     IEnumerator waitForChangeDir(float time)
@@ -63,11 +51,10 @@ public class EnemyMover : MonoBehaviour
             while (!NavMesh.SamplePosition(randomPoint, out hit, 1, NavMesh.AllAreas))
             {
                 randomPoint = new Vector3(Random.Range(lvlSize.x, -lvlSize.x), Random.Range(lvlSize.y, -lvlSize.y), Random.Range(lvlSize.z, -lvlSize.z));
-            } //not sure about that one
+            }
             goal = hit.position;
         }
         ag.SetDestination(goal);
         readyToChangeDestination = true;
     }
-
 }
